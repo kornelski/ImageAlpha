d28c3066b086deac2f2bdb2b04afece2fac5395b
porneL <pornel@pornel.net>
2012-03-13 00:54:47 +0000

    Don't draw rect

--- IABackgroundRenderer.py
+++ IABackgroundRenderer.py
@@ -30,12 +30,6 @@ class IAColorBackgroundRenderer(IABackgroundRenderer):
 		layer.setContents_(None);
 		return layer;
 
-	def drawRect_(self,rect):
-		if self.color is None: return
-
-		self.color.set()
-		NSRectFill(rect)
-
 class IAImageBackgroundRenderer(NSClassFromString("IAPatternBackgroundRenderer")):
 	backgroundImage = None
 	backgroundOffset = (0,0)
@@ -59,24 +53,4 @@ class IAImageBackgroundRenderer(NSClassFromString("IAPatternBackgroundRenderer")
 		self.backgroundOffset = ((self.backgroundOffset[0] + delta[0]),
 								 (self.backgroundOffset[1] + delta[1]))
 		self.tileLayer_atX_Y_(self._bgLayer,self.backgroundOffset[0],self.backgroundOffset[1]);
-
-
-	def drawRect_(self,rect):
-		size = self.backgroundImage.size()
-
-		widthend = rect.origin.x + rect.size.width
-		heightend = rect.origin.y + rect.size.height
-
-		currentx = rect.origin.x // size.width * size.width  - self.backgroundOffset[0]
-		currenty = rect.origin.y // size.height * size.height  - self.backgroundOffset[1]
-		wholeimage = ((0,0),size)
-
-		# totally depends on view doing the clipping anyway
-		while currenty < heightend:
-			drawnwidth = 0
-			currentx = rect.origin.x // size.width * size.width - self.backgroundOffset[0]
-			while currentx < widthend:
-				self.backgroundImage.drawInRect_fromRect_operation_fraction_(((currentx,currenty),size), wholeimage, NSCompositeCopy, 1.0)
-				currentx += size.width
-			currenty += size.height
 
