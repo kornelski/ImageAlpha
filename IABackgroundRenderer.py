37cc9c9405ee67b11d85f06cf37d947548bec481
porneL <pornel@pornel.net>
2012-09-11 21:01:45 +0100

    Solid color renderer

--- IABackgroundRenderer.py
+++ IABackgroundRenderer.py
@@ -8,43 +8,17 @@ from  math import floor, ceil
 from Quartz.CoreGraphics import *
 from Quartz.QuartzCore import *
 
-
-class IABackgroundRenderer(object):
-	def moveBy_(self,by):
-		pass
-
-	def canMove(self):
-		return NO
-
-class IAColorBackgroundRenderer(IABackgroundRenderer):
-	color = None;
-	backgroundImage = None;
-
-	def __init__(self,nscolor):
-		self.color = nscolor
-
-	def getLayer(self):
-		layer = CALayer.layer()
-		# fixme: bad colorspace
-		layer.setBackgroundColor_(CGColorCreateGenericRGB(self.color.redComponent(),self.color.greenComponent(),self.color.blueComponent(),1));
-		layer.setContents_(None);
-		return layer;
+IAColorBackgroundRenderer = NSClassFromString("IAColorBackgroundRenderer");
 
 class IAImageBackgroundRenderer(NSClassFromString("IAPatternBackgroundRenderer")):
-	backgroundImage = None
 	backgroundOffset = (0,0)
 
 	def initWithImage_(self,image):
 		self = super(IAImageBackgroundRenderer, self).init();
 		if self:
-			self.backgroundImage = image
-			self._bgLayer = CALayer.layer();
 			self.setTileImage_(image);
 			self.moveBy_((0,0));
 		return self;
-
-	def getLayer(self):
-		return self._bgLayer;
 
 	def canMove(self):
 		return YES
@@ -52,5 +26,5 @@ class IAImageBackgroundRenderer(NSClassFromString("IAPatternBackgroundRenderer")
 	def moveBy_(self,delta):
 		self.backgroundOffset = ((self.backgroundOffset[0] + delta[0]),
 								 (self.backgroundOffset[1] + delta[1]))
-		self.tileLayer_atX_Y_(self._bgLayer,self.backgroundOffset[0],self.backgroundOffset[1]);
+		self.tileLayerAtX_Y_(self.backgroundOffset[0],self.backgroundOffset[1]);
 
